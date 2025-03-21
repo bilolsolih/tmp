26f2ce64bd932510fc7b1a382e1a417baf29ce15
Maknun <Ubaydulloh950@gmail.com>
2025-03-21 18:16:13 +0500

    Maknun changed

---
+++ lib/core/temp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class Temp extends StatelessWidget {
+  const Temp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
