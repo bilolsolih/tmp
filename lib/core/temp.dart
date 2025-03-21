ebb558c641c02f75dd7e38fe39ef535d0019ce6b
Maknun <Ubaydulloh950@gmail.com>
2025-03-21 18:32:43 +0500

    Maknun djdjdjdd

--- lib/core/temp.dart
+++ lib/core/temp.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 
 
 
 class Temp extends StatelessWidget {
   const Temp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              child: Builder(
+                builder: (context) {
+                  return const Placeholder();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class MaknunMen extends StatelessWidget {
+  const MaknunMen({super.key});
 
   @override
   Widget build(BuildContext context) {
